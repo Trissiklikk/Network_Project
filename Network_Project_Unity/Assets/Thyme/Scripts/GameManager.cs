53ecfec6a5956dd5c495e739398799d849a8a549
Anak Ladachat <70420610+Trissiklikk@users.noreply.github.com>
2021-05-06 00:34:06 +0700

    .

---
+++ Network_Project_Unity/Assets/Thyme/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetworkGame;
+
+public class GameManager : MonoBehaviour
+{
+    public void OnGUI()
+    {
+        if (GUILayout.Button("Connect"))
+        {
+            
+        }
+
+    }
+}
