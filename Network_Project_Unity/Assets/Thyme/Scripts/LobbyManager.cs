d0178171645cd600d54fa075e49a84a447687268
Anak Ladachat <70420610+Trissiklikk@users.noreply.github.com>
2021-05-07 15:22:04 +0700

    Create & Join room
    Create & Join room

---
+++ Network_Project_Unity/Assets/Thyme/Scripts/LobbyManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+using Photon.Realtime;
+public class LobbyManager : PunBehaviour
+{
+    public string characterPrefName;
+    public Transform spawnPoint;
+
+    private GameObject ownerCharacter;
+    public Text dubugForPlayer;
+    public Text roomNameInput;
+    public Text playerNameInput;
+    public GameObject lobbyUI;
+    private void Start()
+    {
+
+        if (PhotonNetwork.connected == false)
+        {
+            PhotonNetwork.ConnectUsingSettings("1.0");
+            //PhotonNetwork.playerName = playerName;
+        }
+
+
+    }
+
+    public void OnGUI()
+    {
+        if (PhotonNetwork.insideLobby)
+        {
+            for (int i = 0; i < PhotonNetwork.GetRoomList().Length; i++)
+            {
+                RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+                if (GUILayout.Button("JoinRoom : " + rooms[i].Name))
+                {
+                    PhotonNetwork.JoinRoom(rooms[i].Name);
+                }
+            }
+        }
+        else if (PhotonNetwork.inRoom)
+        {
+            if (ownerCharacter == null)
+            {
+                if (GUILayout.Button("SpawnCharacter"))
+                {
+                    ownerCharacter = PhotonNetwork.Instantiate(characterPrefName, spawnPoint.position, spawnPoint.rotation, 0);
+                }
+            }
+
+        }
+
+    }
+
+    public void Update()
+    {
+
+        dubugForPlayer.text = PhotonNetwork.connectionState.ToString();
+
+
+
+
+
+    }
+
+
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer player)
+    {
+        Debug.Log("Player : " + player.NickName + " is connected");
+    }
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer player)
+    {
+        Debug.Log("Player : " + player.NickName + " is disconnected");
+    }
+
+    public void ButtonCreateJoin()
+    {
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        PhotonNetwork.JoinOrCreateRoom(roomNameInput.text, roomOptions, TypedLobby.Default);
+        lobbyUI.SetActive(false);
+
+
+
+
+    }
+
+
+}
+
---
+++ Network_Project_Unity/Assets/Thyme/Scripts/PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Realtime;
+
+public class PlayerMovement : PunBehaviour, IPunObservable
+{
+    public float moveSpeed = 5.0f;
+    public float gravity = 10.0f;
+    public float cameraDistance = 10.0f;
+    public float cameraHeight = 5.0f;
+    public float cameraAngleLook = 30.0f;
+    public float cameraSensitivity = 10.0f;
+
+    private Vector3 velocity;
+    private Vector3 dir;
+    private CharacterController characterController;
+
+    private Vector3 correctPosition;
+    private Quaternion correctRotation;
+
+    private Camera camera;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.isMine)
+        {
+            Movement();
+            UpdateCamera();
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, correctPosition, 5.0f * Time.deltaTime);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, correctRotation, 5.0f * Time.deltaTime);
+        }
+    }
+
+    void Movement()
+    {
+        dir = (this.transform.forward * Input.GetAxis("Vertical"))+
+            (this.transform.right * Input.GetAxis("Horizontal"));
+
+        dir.Normalize();
+
+        if (characterController.isGrounded)
+        {
+            velocity = Vector3.zero;
+
+            velocity += dir * moveSpeed;
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void UpdateCamera()
+    {
+        if(camera == null)
+        {
+            camera = Camera.main;
+        }
+        else
+        {
+            if(camera.transform.parent != this.transform)
+            {
+                camera.transform.parent = this.transform;
+                camera.transform.localPosition = (-camera.transform.forward * cameraDistance) +
+                    (camera.transform.up * cameraHeight);
+            }
+            else
+            {
+                //camera.transform.localEulerAngles = new Vector3(cameraAngleLook, 0.0f, 0.0f);
+            }
+        }
+
+        this.transform.RotateAround(this.transform.position, Vector3.up, Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime);
+    }
+
+   public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(this.transform.position);
+            stream.SendNext(this.transform.rotation);
+        }
+        else
+        {
+            correctPosition = (Vector3)stream.ReceiveNext();
+            correctRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
